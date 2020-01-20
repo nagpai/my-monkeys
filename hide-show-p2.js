51b1d51dcb3fd2c8e9dc509a8691f1897e8276a8
nagpai <nagesh.pai@gmail.com>
2020-01-20 20:06:29 +0530

    Changes to be committed: Updated script to remove JS errors and also a new way to identify p2s modified:   hide-show-p2.js

--- hide-show-p2.js
+++ hide-show-p2.js
@@ -3,66 +3,62 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       Nagesh
+// @author       You
 // @match        https://*.wordpress.com/
 // @grant        none
 // ==/UserScript==
 
-if (document.querySelector('body').classList[7] === 'o2') { //check if the site is a p2
+if ([...document.querySelector('body').classList].includes("o2")) {
 
-    let menuBar = document.getElementById('wp-admin-bar-root-default');
-
+    const menuBar = document.getElementById('wp-admin-bar-root-default');
     //create Hide Show comments button
-    let hideCommentsButton = document.createElement('li');
-    hideCommentsButton.innerHTML = '<a id="hide-comments" style="cursor: pointer"> Hide Comments </a>';
+    const hideCommentsButton = document.createElement('li');
+    hideCommentsButton.innerHTML = '<a class="ab-item" style="cursor: pointer" id="hide-comments"> Hide Comments </a>';
     hideCommentsButton.onclick = hideShowComments;
     menuBar.appendChild(hideCommentsButton);
 
     //create Hide Show content
-    let hideContentsButton = document.createElement('li');
-    hideContentsButton.innerHTML = '<a id="hide-contents" style="cursor: pointer"> Hide Contents </a>';
+    const hideContentsButton = document.createElement('li');
+    hideContentsButton.innerHTML = '<a class="ab-item" style="cursor: pointer" id="hide-contents"> Hide Contents </a>';
     hideContentsButton.onclick = hideShowContents;
     menuBar.appendChild(hideContentsButton);
 }
 
 //show and hide comments function
 function hideShowComments (event) {
-    let commentText = document.getElementById('hide-comments');
-    let allComments = document.getElementsByClassName('o2-post-comments');
+    const commentText = document.getElementById('hide-comments');
+    const allComments = document.getElementsByClassName('o2-post-comments');
 
     if ( allComments[1].style.display !== 'none'){
 
-        for ( let i = 0; i <= allComments.length; i++) {
-            allComments[i].style.display = 'none';
-        }
-
-        console.log(commentText.innerText);
-
-        commentText.innerText = 'Show Comments';
+         for ( let i = 0; i <= allComments.length; i++) {
+             if(allComments[i]) {allComments[i].style.display = 'none';}
+         }
+        event.target.innerText = 'Show Comments';
 
     } else {
         for ( let i = 0; i <= allComments.length; i++) {
-            allComments[i].style.display = 'block';
+            if(allComments[i]){allComments[i].style.display = 'block';}
         }
-        commentText.innerText = 'Hide Comments';
+        event.target.innerText = 'Hide Comments';
     }
 }
 
 //show and hide main content function
 function hideShowContents (event) {
-    let contentText = document.getElementById('hide-contents');
+
     let allContents = document.getElementsByClassName('entry-content');
     if( allContents[1].style.display !== 'none'){
         for( let i = 1; i <= allContents.length; i++){
-            allContents[i].style.display = 'none';
+            if(allContents[i]) {allContents[i].style.display = 'none';}
         }
-        contentText.innerText = 'Show Contents';
+        event.target.innerText = 'Show Content';
     } else {
         for( let i = 1; i <= allContents.length; i++){
-            allContents[i].style.display = '';
+            if(allContents[i]){allContents[i].style.display = '';}
         }
-        contentText.innerText = 'Hide Contents';
+        event.target.innerText = 'Hide Content';
+
     }
 
 }
- 
